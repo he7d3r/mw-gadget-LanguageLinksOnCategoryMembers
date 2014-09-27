b930442329c941bb325fb284557106b497d442fc
Helder <he7d3r@gmail.com>
2014-09-27 20:49:01 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/LanguageLinksOnCategoryMembers.js
+++ src/LanguageLinksOnCategoryMembers.js
@@ -4,53 +4,53 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-var langCode = 'pt';
+	var langCode = 'pt';
 
-function addLanguageLinks ( data ){
-	var map = {};
-	$.each( data.query.pages, function( i ){
-		if ( data.query.pages[ i ].langlinks ){
-			map[ data.query.pages[ i ].title ] = data.query.pages[ i ].langlinks[0]['*'];
-		}
-	} );
-	$( '#mw-pages' ).find( 'a' ).each(function(){
-		var $this = $(this),
-			title = $this.text();
-		if( map[ title ] ){
-			$this
-				.before( '[' )
-				.before(
-					$( '<a></a>' ).attr( {
-						title: map[ title ],
-						href: '//' + langCode + '.wikipedia.org/wiki/' +
-							mw.util.rawurlencode( map[ title ] ),
-						lang: langCode,
-						hreflang: langCode
-					} ).text( langCode )
-				)
-				.before( '] ' );
-		}
-	});
-}
+	function addLanguageLinks( data ) {
+		var map = {};
+		$.each( data.query.pages, function ( i ) {
+			if ( data.query.pages[ i ].langlinks ) {
+				map[ data.query.pages[ i ].title ] = data.query.pages[ i ].langlinks[0]['*'];
+			}
+		} );
+		$( '#mw-pages' ).find( 'a' ).each(function () {
+			var $this = $(this),
+				title = $this.text();
+			if ( map[ title ] ) {
+				$this
+					.before( '[' )
+					.before(
+						$( '<a></a>' ).attr( {
+							title: map[ title ],
+							href: '//' + langCode + '.wikipedia.org/wiki/' +
+								mw.util.rawurlencode( map[ title ] ),
+							lang: langCode,
+							hreflang: langCode
+						} ).text( langCode )
+					)
+					.before( '] ' );
+			}
+		});
+	}
 
-function getCategoryMembers (){
-	var api = new mw.Api();
-	api.get( {
-		prop: 'langlinks',
-		lllang: langCode,
-		generator: 'categorymembers',
-		gcmtitle: mw.config.get( 'wgPageName' ),
-		gcmlimit: 500
-	} )
-	.done( addLanguageLinks );
-}
+	function getCategoryMembers() {
+		var api = new mw.Api();
+		api.get( {
+			prop: 'langlinks',
+			lllang: langCode,
+			generator: 'categorymembers',
+			gcmtitle: mw.config.get( 'wgPageName' ),
+			gcmlimit: 500
+		} )
+		.done( addLanguageLinks );
+	}
 
-if( mw.config.get( 'wgNamespaceNumber' ) === 14 && mw.config.get( 'wgAction' ) === 'view' && mw.config.get( 'wgContentLanguage' ) !== langCode ){
-	mw.loader.using( [ 'mediawiki.api', 'mediawiki.util' ], function(){
-		$( getCategoryMembers );
-	} );
-}
+	if ( mw.config.get( 'wgNamespaceNumber' ) === 14 && mw.config.get( 'wgAction' ) === 'view' && mw.config.get( 'wgContentLanguage' ) !== langCode ) {
+		mw.loader.using( [ 'mediawiki.api', 'mediawiki.util' ], function () {
+			$( getCategoryMembers );
+		} );
+	}
 
 }( mediaWiki, jQuery ) );
